578fc1ec5a34670a87b6ed8f98abcd746d600609
aldo-pereira22 <aldopereira.gyn@gmail.com>
2020-08-02 00:22:08 -0300

    ajuste do build

--- new-app/src/App.js
+++ new-app/src/App.js
@@ -1,33 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  constructor(props){
-    super(props);
-    this.state = { 
-      showImage:false;
+
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      showImage:false
     }
   }
-  return (
-    <div className="App">
-      <header className="App-header">
-        <button> Mostrar</button>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  toggle = () => {
+    this.setState({
+      showImage: !this.setState.showImage,
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <button type="button" onClick={this.toggle} > Mostrar</button>
+          {
+            this.state.img && (
+              <img src={logo} className="App-logo" alt="logo" />
+            )
+          }
+
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+
+
 }
 
 export default App;
