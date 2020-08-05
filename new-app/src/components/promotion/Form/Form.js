48e9bb9011f1b0fef2634fb66e3744308a9a8a3c
aldo-pereira22 <aldopereira.gyn@gmail.com>
2020-08-05 12:57:13 -0300

    cadastro-promoção

--- new-app/src/components/promotion/Form/Form.js
+++ new-app/src/components/promotion/Form/Form.js
@@ -1,35 +1,67 @@
-import React from 'react'
+import React, {useState} from 'react';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
 import './Form.css'
+
+
+const initialValue = {
+    title: '',
+    url: '',
+    imageUrl: '',
+    price: 0
+}
 
 const PromotionForm = () => {
      
+    const [values, setValues] = useState(initialValue);
+    const history = useHistory();
+
+    function onChange(ev){
+        const {name, value} = ev.target
+        console.log({name, value})
+
+        setValues =( {...values, [name]:value })
+
+    }
+
+    function onSubmit(ev){
+        ev.preventDefault();
+
+        axios.post('http://localhost:5000/promotions', values)
+            .then( (response) => {
+            history.push('/'); 
+
+            })
+
+    }
+
     return (
         <div> 
             <h1> Promo Show </h1>
             <h2> Nova Promoção </h2>
 
-            <form> 
+            <form onSubmit={ onSubmit }> 
                 <div className="promotion-form__group"> 
                     <label htmlFor="title "> Título </label>
-                    <input id="title" name="title" type="text" />
+                    <input id="title" name="title" type="text" onChange={onChange} />
 
                 </div>
 
                 <div className="promotion-form__group"> 
                     <label htmlFor="url "> Link </label>
-                    <input id="url" name="url" type="text" />
+                    <input id="url" name="url" type="text" onChange={onChange} />
 
                 </div>
 
                 <div className="promotion-form__group"> 
                     <label htmlFor="imageUrl "> Imagem (url) </label>
-                    <input id="imageUrl" name="imageUrl" type="text" />
+                    <input id="imageUrl" name="imageUrl" type="text" onChange={onChange} />
 
                 </div>
 
                 <div className="promotion-form__group"> 
                     <label htmlFor="price "> Preço </label>
-                    <input id="price" name="price" type="number" />
+                    <input id="price" name="price" type="number" onChange={onChange} />
 
                 </div>
 
