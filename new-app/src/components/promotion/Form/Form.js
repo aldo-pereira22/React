27e23a28826ea01518e1b5626aa6a4f1133006ff
aldo-pereira22 <aldopereira.gyn@gmail.com>
2020-08-05 12:01:31 -0300

    formulario-implementado

--- new-app/src/components/promotion/Form/Form.js
+++ new-app/src/components/promotion/Form/Form.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Form.css'
 
 const PromotionForm = () => {
      
@@ -6,7 +7,41 @@ const PromotionForm = () => {
         <div> 
             <h1> Promo Show </h1>
             <h2> Nova Promoção </h2>
+
+            <form> 
+                <div className="promotion-form__group"> 
+                    <label htmlFor="title "> Título </label>
+                    <input id="title" name="title" type="text" />
+
+                </div>
+
+                <div className="promotion-form__group"> 
+                    <label htmlFor="url "> Link </label>
+                    <input id="url" name="url" type="text" />
+
+                </div>
+
+                <div className="promotion-form__group"> 
+                    <label htmlFor="imageUrl "> Imagem (url) </label>
+                    <input id="imageUrl" name="imageUrl" type="text" />
+
+                </div>
+
+                <div className="promotion-form__group"> 
+                    <label htmlFor="price "> Preço </label>
+                    <input id="price" name="price" type="number" />
+
+                </div>
+
+                <div> 
+                    <button type="submit">
+                        Salvar
+                    </button>
+                </div>
+            </form>
+
         </div>
+
     )
 }
 
